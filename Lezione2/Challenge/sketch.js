b1d8def6e049e6681274c4d13f73b9f628946db9
Simomeno <menonisimone04@gmail.com>
2024-10-04 21:01:06 +0200

    prove

--- Lezione2/Challenge/sketch.js
+++ Lezione2/Challenge/sketch.js
@@ -1,32 +1,149 @@
-let xoff = 0;
-let yoff = 0;
-let inc = 0.01;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  
 }
 
 function draw() {
   background(0);
-  translate(0, height/2);
-  rotateX(PI/2);
+  orbitControl();
+  lights()
+  fill(140, 130, 50)
+  rotateX(QUARTER_PI)
+  rotateY(QUARTER_PI/-2)
+  
+  scale(2.3)
+  fill(150, 120, 10)
+  torus(30, 20, 100, 100);
+  
+  //
+  fill(101,67,33)
+  translate(0, 0, 7)
+  scale(1.03)
+  push();
+  torus(30, 20, 200, 20);
+  pop();
 
-  for (let y = -height/2; y < height/2; y += 10) {
-    beginShape(TRIANGLE_STRIP);
-    for (let x = -width/2; x < width/2; x += 10) {
-      let yoff = y * inc;
-      let xoff = x * inc;
-      let n = noise(xoff, yoff);
-      let h = map(n, 0, 1, -50, 50);
-      vertex(x, y, h);
-      yoff += inc;
-      n = noise(xoff, yoff);
-      h = map(n, 0, 1, -50, 50);
-      vertex(x, y+10, h);
-    }
-    endShape();
-  }
+  //
+  fill("white")
+  noStroke()
+  translate(16,20,20)
+  scale(0.08)
+  cylinder(10, 40);
+  //
+  noStroke()
+  translate(90,100,-7)
+  rotateZ(90)
+  cylinder(10, 25);
+  //
+  noStroke()
+  translate(90,100,0)
+  rotateZ(90)
+  cylinder(10, 30);
 
-  xoff += inc;
-  yoff += inc;
+
+   //aggiunti con control c
+   fill("white")
+   
+   translate(700,20,20)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateZ(90)
+   cylinder(10, 30);
+   
+   //aggiunti con control c
+   fill("white")
+   
+   translate(400,20,0)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateZ(90)
+   cylinder(10, 30);
+   
+
+   //aggiunti con control c
+   fill("white")
+   
+   translate(700,20,0)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateZ(90)
+   cylinder(10, 30);
+   //aggiunti con control c
+   fill("white")
+   
+   translate(400,100,2)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateZ(90)
+   cylinder(10, 30);
+   //aggiunti con control c
+   fill("white")
+   
+   translate(700,200,15)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateZ(90)
+   cylinder(10, 30);
+   //aggiunti con control c
+   fill("white")
+   
+   translate(500,200,0)
+   
+   cylinder(10, 40);
+   //
+   noStroke()
+   translate(90,100,-7)
+   rotateZ(90)
+   cylinder(10, 25);
+   //
+   noStroke()
+   translate(90,100,0)
+   rotateX(90)
+   cylinder(10, 30);
+ 
 }
+
+
+
+
+
